9bdb504279fe6d0df445707a010943eaf338efa2
Bernardo <bernardo.fonpac@gmail.com>
2020-04-12 16:06:13 -0300

    tchau

---
+++ WrigGame/Assets/Scripts/characterStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterStats : MonoBehaviour
+{
+    public float baseDamage = 20;
+
+    public float baseHealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void modifyDamage(float modifier) {
+        baseDamage += modifier * baseDamage;
+    }
+
+    void modifyHealth(float modifier) {
+        baseHealth += modifier * baseHealth;
+    }
+}
